3e80bfc0d6b967712ada9260f4c14feed2989ab5
habibaudu <auduhabib1990@gmail.com>
2019-01-09 11:24:31 +0100

    [#162727542] cleanup - convert config to js file - refactor models index file - add env to gitignore

--- config/passport.js
+++ config/passport.js
@@ -1,23 +1,24 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-import User from '../models'
+import passport from 'passport';
+import User from '../models';
+
+const LocalStrategy = require('passport-local').Strategy;
 
 passport.use(
-    new LocalStrategy(
-        {
-            usernameField: "user[email]",
-            passwordField: "user[password]"
-        },
-        function(username, password, done) {
-            User.findOne({ username: username }, function(err, user) {
-              if (err) { return done(err); }
-              if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
-              }
-              if (!user.validPassword(password)) {
-                return done(null, false, { message: 'Incorrect password.' });
-              }
-              return done(null, user);
-            });
-          }
-));
+  new LocalStrategy(
+    {
+      usernameField: 'user[email]',
+      passwordField: 'user[password]'
+    },
+    ((username, password, done) => {
+      User.findOne({ username }, (err, user) => {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    })
+  ));
