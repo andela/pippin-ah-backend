42545a00eaf243b05ec0464b2a981007e6eea8bf
habibaudu <auduhabib1990@gmail.com>
2019-01-09 12:51:40 +0100

    bug(start-script):fix bug on start script [#163085204]

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
-import dbConfig from '../config/config';
+import databaseConfig from '../config/config';
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = dbConfig[env];
+const config = databaseConfig[env];
 
 const db = {};
 
