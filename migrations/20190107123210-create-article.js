b76676d724436ab587244075bbd59ec05158e237
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-11 08:26:22 +0100

    feat(articles model): create articles model - ensure relationship with users - ensure relationship with comments [Finishies #163114855]

---
+++ migrations/20190107123210-create-article.js
@@ -0,0 +1,50 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('articles', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    post: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    likes: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    },
+    dislikes: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    },
+    rating: {
+      type: Sequelize.INTEGER,
+      allowNull: true
+    },
+    category: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'users',
+        key: 'id',
+        as: 'userId'
+      }
+    },
+
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('articles')
+};
---
+++ models/article.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const Article = sequelize.define('Article', {
+    post: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    dislikes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
+  });
+
+  Article.associate = (models) => {
+    Article.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+
+    Article.hasMany(models.Comment, {
+      foreignKey: 'articleId',
+      onDelete: 'CASCADE'
+    });
+  };
+  return Article;
+};
