8d599785acfbea29cd74faec589782f61ddbfcc9
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-11 11:00:27 +0100

    add test suite for 404 route

--- test/default.js
+++ test/default.js
@@ -1,11 +1,8 @@
-import { assert } from 'chai';
+import { expect } from 'chai';
 
 describe('Basic Test to Verify Mocha', () => {
   it('should add two numbers correctly', () => {
     const x = 5 + 5;
-    assert(x === 10, 'x should equal 10');
-  });
-  it('should return first character of the string', () => {
-    assert('Welcome'.charAt(0) === 'W', 'First in Welcome character is W');
+    expect(x).to.equal(10);
   });
 });
