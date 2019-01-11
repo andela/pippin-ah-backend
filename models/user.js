bbfcea659d6faf54e22a55240ed86ecae8c33296
habibaudu <auduhabib1990@gmail.com>
2019-01-11 09:25:44 +0100

    fixing conflict

--- models/user.js
+++ models/user.js
@@ -2,10 +2,13 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
+      allowNull: false,
       unique: true
+
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
       unique: true
     },
     password: {
