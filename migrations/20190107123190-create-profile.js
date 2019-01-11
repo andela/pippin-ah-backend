93403975ceba10a14ade90083907c13d885dd298
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-11 12:18:55 +0100

    refactor model names

--- migrations/20190107123190-create-profile.js
+++ migrations/20190107123190-create-profile.js
@@ -23,7 +23,7 @@ module.exports = {
       allowNull: false,
       onDelete: 'CASCADE',
       references: {
-        model: 'users',
+        model: 'Users',
         key: 'id',
         as: 'userId'
       }
