b9dfa6b5f6ddd851cfdcc6f619e793f3dec86a96
habibaudu <auduhabib1990@gmail.com>
2019-01-11 16:10:51 +0100

    feat(resgistration errors):write tests for errors [finish #162727552]

--- test/user.test.js
+++ test/user.test.js
@@ -1,35 +1,90 @@
-import { expect } from 'chai';
-import request from 'supertest';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+
 import 'babel-polyfill';
 import server from '../app';
 
-describe('User', () => {
-  it('password must contain only numbers and alphabet', (done) => {
-    const newUser2 = {
-      username: 'habibaudu',
-      email: 'auduhabib1990@gmail.com',
-      password: 'hhrt----',
-    };
-    request(server)
-      .post('/api/v1/users')
-      .send(newUser2)
-      .end((err, res) => {
-        expect(res.body).to.equal(
-          'password must contain only numbers and alphabet');
-        done();
-      });
-  });
+chai.use(chaiHttp);
+
+describe('Test suite for post/user', () => {
+  it('should return password must contain only numbers and alphabet',
+    (done) => {
+      const newUser2 = {
+        username: 'habibaudu',
+        email: 'auduhabib1990@gmail.com',
+        password: 'hhrt----',
+      };
+      chai.request(server)
+        .post('/api/v1/users')
+        .send(newUser2)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.errors.body[0]).to.equal(
+            'password must contain only numbers and alphabet');
+          done();
+        });
+    });
+  it('should return status code of 201',
+    (done) => {
+      const newUser2 = {
+        username: 'habibaudu',
+        email: 'auduhabib1990@gmail.com',
+        password: 'hhrtuyhgt678',
+      };
+      chai.request(server)
+        .post('/api/v1/users')
+        .send(newUser2)
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          done();
+        });
+    });
+  it('should return Email already in use',
+    (done) => {
+      const newUser2 = {
+        username: 'habibaudu',
+        email: 'auduhabib1990@gmail.com',
+        password: 'hhrtuyhgt678',
+      };
+      chai.request(server)
+        .post('/api/v1/users')
+        .send(newUser2)
+        .end((err, res) => {
+          expect(res.status).to.equal(409);
+          expect(res.body.errors.body[0]).to.equal(
+            'Email already in use');
+          done();
+        });
+    });
+  it('should return Email already in use',
+    (done) => {
+      const newUser2 = {
+        username: 'habibaudu',
+        email: 'auduhabib@gmail.com',
+        password: 'hhrtuyhgt678',
+      };
+      chai.request(server)
+        .post('/api/v1/users')
+        .send(newUser2)
+        .end((err, res) => {
+          expect(res.status).to.equal(409);
+          expect(res.body.errors.body[0]).to.equal(
+            'Username already in use');
+          done();
+        });
+    });
   it('should return Your password must be at least 8 characters', (done) => {
     const newUser2 = {
       username: 'habibaudu',
       email: 'auduhabib1990@gmail.com',
       password: 'hba123',
     };
-    request(server)
+    chai.request(server)
       .post('/api/v1/users')
       .send(newUser2)
       .end((err, res) => {
-        expect(res.body).to.equal(
+        expect(res.status).to.equal(400);
+        expect(res.body.errors.body[0]).to.equal(
           'Your password must be at least 8 characters');
         done();
       });
@@ -41,11 +96,12 @@ describe('User', () => {
       email: 'auduhabib1990@gmail.com',
       password: 'hbasdg3546',
     };
-    request(server)
+    chai.request(server)
       .post('/api/v1/users')
       .send(newUser2)
       .end((err, res) => {
-        expect(res.body).to.equal(
+        expect(res.status).to.equal(400);
+        expect(res.body.errors.body[0]).to.equal(
           'Your username must be at least 6 characters');
         done();
       });
@@ -57,28 +113,30 @@ describe('User', () => {
         email: 'auduhabib1990@gmail.com',
         password: 'hbasdg3546',
       };
-      request(server)
+      chai.request(server)
         .post('/api/v1/users')
         .send(newUser2)
         .end((err, res) => {
-          expect(res.body).to.equal(
+          expect(res.status).to.equal(400);
+          expect(res.body.errors.body[0]).to.equal(
             'username must contain only alphabets and numbers');
           done();
         });
     });
 
-  it('should return username field is required', (done) => {
+  it('should username must contain only alphabets and numbers', (done) => {
     const newUser2 = {
       username: '',
       email: 'auduhabib1990@gmail.com',
       password: 'hbasdg3546',
     };
-    request(server)
+    chai.request(server)
       .post('/api/v1/users')
       .send(newUser2)
       .end((err, res) => {
-        expect(res.body).to.equal(
-          'username field is required');
+        expect(res.status).to.equal(400);
+        expect(res.body.errors.body[0]).to.equal(
+          'username must contain only alphabets and numbers');
         done();
       });
   });
@@ -88,27 +146,13 @@ describe('User', () => {
       email: 'auduhabib19gmail.com',
       password: 'hbasdg3546',
     };
-    request(server)
+    chai.request(server)
       .post('/api/v1/users')
       .send(newUser2)
       .end((err, res) => {
-        expect(res.body).to.equal(
+        expect(res.status).to.equal(400);
+        expect(res.body.errors.body[0]).to.equal(
           'please Enter a valid Email');
-        done();
-      });
-  });
-  it('should return All fields are required', (done) => {
-    const newUser2 = {
-      username: '',
-      email: '',
-      password: '',
-    };
-    request(server)
-      .post('/api/v1/users')
-      .send(newUser2)
-      .end((err, res) => {
-        expect(res.body.error.body[0]).to.equal(
-          'All fields are required');
         done();
       });
   });
