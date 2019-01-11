3408d5c5579ab33d60fe1e67c7a09f710e0f08dc
habibaudu <auduhabib1990@gmail.com>
2019-01-11 09:16:34 +0100

    feat()pulled from develop

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,2 +1,3 @@
 export { default as errorHandler } from './errorHandler';
 export { default as notFoundRoute } from './notFoundRoute';
+export { default as userValidation } from './userValidations';
