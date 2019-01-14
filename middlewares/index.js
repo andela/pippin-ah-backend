d1accaa0751021245c23426ab7965c3b913fc437
Ebenezer <ebenezerdonu@gmail.com>
2019-01-14 16:49:18 +0100

    Add verify token to index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,4 @@
 export { default as errorHandler } from './errorHandler';
 export { default as userValidations } from './userValidations';
 export { default as notFoundRoute } from './notFoundRoute';
+export { verifyToken } from './authentication';
