b62980586f77b569056a9ea96543ee0a9b33dd53
Ebenezer <ebenezerdonu@gmail.com>
2019-01-16 18:02:16 +0100

    [#163224026]: Refactor twitterStrategy.js

---
+++ config/strategies/twitterStrategy.js
@@ -0,0 +1,23 @@
+import passport from 'passport';
+import Strategy from 'passport-twitter';
+import Users from '../../controllers';
+
+const { processTwitterUser } = Users;
+
+passport.use(new Strategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: '/api/v1/users/twitter/redirect'
+},
+(accessToken, refreshToken, profile, done) => {
+  console.log(process.env.TWITTER_CONSUMER_KEY);
+  console.log('==========', profile);
+  done(null, {
+    email: profile.email
+  });
+}));
+
+const twitterAuthenticate = () => passport.authenticate('twitter');
+
+console.log(Strategy);
+console.log(processTwitterUser);
