65580ec1bc4652568bf61cce78c783bf93232850
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-16 12:05:21 +0100

    add test for google redirect

--- test/auth.test.js
+++ test/auth.test.js
@@ -9,34 +9,44 @@ describe('AUTHENTICATION TEST SUITE', () => {
   before(() => models.sequelize.sync({ force: true }));
 
   describe('JWT AUTHENTICATION', () => {
-    it('should return a token on successful registration',
-      (done) => {
-        const newUser2 = {
-          username: 'ebenezer',
-          email: 'ebenezer@gmail.com',
-          password: 'secretstuff',
-        };
-        chai.request(server)
-          .post('/api/v1/users')
-          .send(newUser2)
-          .end((err, res) => {
-            expect(res.body.token).to.not.equal(undefined);
-            done();
-          });
-      });
-    it('should return a token on successful login',
-      (done) => {
-        const newUser2 = {
-          usernameOrEmail: 'ebenezer',
-          password: 'secretstuff',
-        };
-        chai.request(server)
-          .post('/api/v1/users/login')
-          .send(newUser2)
-          .end((err, res) => {
-            expect(res.body.token).to.not.equal(undefined);
-            done();
-          });
-      });
+    it('should return a token on successful registration', (done) => {
+      const newUser2 = {
+        username: 'ebenezer',
+        email: 'ebenezer@gmail.com',
+        password: 'secretstuff'
+      };
+      chai
+        .request(server)
+        .post('/api/v1/users')
+        .send(newUser2)
+        .end((err, res) => {
+          expect(res.body.token).to.not.equal(undefined);
+          done();
+        });
+    });
+    it('should return a token on successful login', (done) => {
+      const newUser2 = {
+        usernameOrEmail: 'ebenezer',
+        password: 'secretstuff'
+      };
+      chai
+        .request(server)
+        .post('/api/v1/users/login')
+        .send(newUser2)
+        .end((err, res) => {
+          expect(res.body.token).to.not.equal(undefined);
+          done();
+        });
+    });
+  });
+
+  describe('GOOGLE AUTHENTICATION', () => {
+    it('should redirect to google login page', async () => {
+      const response = await chai.request(server).get('/api/v1/users/google');
+      const redirectUrlSubstring = 'https://accounts.google.com/o/oauth2';
+      const redirectUrl = response.redirects[0];
+      expect(200);
+      expect(redirectUrl.includes(redirectUrlSubstring)).to.equal(true);
+    });
   });
 });
