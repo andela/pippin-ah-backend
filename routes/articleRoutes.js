58e10d6f93fec21ae3a2e7acc47a200098de941d
Ebenezer <ebenezerdonu@gmail.com>
2019-01-18 18:05:41 +0100

    [#162727556]: Add verifytoken middleware to article route

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import Articles from '../controllers/article';
+import { verifyToken } from '../middlewares';
 
 const router = express.Router();
 const { createArticle } = Articles;
 
 router.route('/articles')
+  .all(verifyToken)
   .post(createArticle);
 
 export default router;
