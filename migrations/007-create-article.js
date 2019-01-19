01d2dd98b61915978d5326320833d94817df7284
Ebenezer <ebenezerdonu@gmail.com>
2019-01-19 16:27:13 +0100

    [#162727556]: remove post of relation articles

--- migrations/007-create-article.js
+++ migrations/007-create-article.js
@@ -2,24 +2,27 @@ const tableName = 'Articles';
 
 module.exports = {
   up: (queryInterface, Sequelize) => Promise.all([
-    queryInterface.removeColumn(tableName, 'post'),
 
     queryInterface.addColumn(tableName, 'title', {
       type: Sequelize.STRING,
       allowNull: false
     }),
+
     queryInterface.addColumn(tableName, 'body', {
       type: Sequelize.STRING,
       allowNull: true
     }),
+
     queryInterface.addColumn(tableName, 'description', {
       type: Sequelize.STRING,
       allowNull: true
     }),
+
     queryInterface.addColumn(tableName, 'tags', {
       type: Sequelize.STRING,
       allowNull: true
     }),
+
     queryInterface.addColumn(tableName, 'slug', {
       type: Sequelize.STRING,
       allowNull: true
