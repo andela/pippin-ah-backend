3040c54ed8d2a06fc04acb4b3a79f83780159f14
Ebenezer <ebenezerdonu@gmail.com>
2019-01-19 16:06:24 +0100

    [#162727556]: Add migration file for table changes

---
+++ migrations/007-create-article.js
@@ -0,0 +1,34 @@
+const tableName = 'Articles';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => Promise.all([
+    queryInterface.removeColumn(tableName, 'post'),
+
+    queryInterface.addColumn(tableName, 'title', {
+      type: Sequelize.STRING,
+      allowNull: false
+    }),
+    queryInterface.addColumn(tableName, 'body', {
+      type: Sequelize.STRING,
+      allowNull: true
+    }),
+    queryInterface.addColumn(tableName, 'description', {
+      type: Sequelize.STRING,
+      allowNull: true
+    }),
+    queryInterface.addColumn(tableName, 'tags', {
+      type: Sequelize.STRING,
+      allowNull: true
+    }),
+    queryInterface.addColumn(tableName, 'slug', {
+      type: Sequelize.STRING,
+      allowNull: true
+    })
+  ]),
+
+  /* eslint-disable no-unused-vars */
+  down: (queryInterface, Sequelize) => Promise.all([
+    queryInterface.removeColumn(tableName, 'slug'),
+    queryInterface.removeColumn(tableName, 'tags')
+  ])
+};
