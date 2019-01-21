0e2564e6de7659f8ce82d28b2be1e0a0d0b4530a
Ebenezer <ebenezerdonu@gmail.com>
2019-01-21 12:01:55 +0100

    [#163357750] create bookmark table

---
+++ migrations/008-create-bookmark-table.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Bookmark', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4
+    },
+    articleId: {
+      type: Sequelize.UUID,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    bookmarkedBy: {
+      type: Sequelize.UUID,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    bookmarked: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Bookmark')
+};
---
+++ models/reaction.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const Reaction = sequelize.define('Reaction', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
+    ReactionId: {
+      type: DataTypes.UUID
+    },
+    likedOrDislikedBy: {
+      type: DataTypes.UUID
+    },
+    liked: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    disliked: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+  Reaction.associate = (models) => {
+    Reaction.belongsTo(models.Article, {
+      foreignKey: 'articleId',
+      onDelete: 'CASCADE'
+    });
+
+    Reaction.belongsTo(models.User, {
+      foreignKey: 'likedOrDislikedBy',
+      onDelete: 'CASCADE'
+    });
+  };
+  return Reaction;
+};
