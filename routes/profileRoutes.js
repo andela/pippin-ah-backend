e82fb5b6e1624ccf34ca2c3424d953a3478271e5
habibaudu <auduhabib1990@gmail.com>
2019-01-22 11:57:51 +0100

    resolve conflict [#162727551]

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -7,17 +7,10 @@ const { updateProfile } = Profile;
 
 const router = express.Router();
 
-<<<<<<< HEAD
-router.route('/')
-  .patch(
-    verifyToken,
-    categoryValidator,
-=======
 router.route('/profile')
   .patch(
     verifyToken,
     interestsValidator,
->>>>>>> c705d6f3cf825d6112fa9a9c9226ab7bf5e08824
     nameValidator,
     updateProfile
   );
