21f73d6544574ede621e7c06d514481e40bddce4
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-22 20:03:42 +0100

    add comment route

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,20 +1,27 @@
 import express from 'express';
-import { Article } from '../controllers';
+import { Article, Comment } from '../controllers';
 import {
   verifyToken,
   articleValidation,
-  profileValidations
+  profileValidations,
+  commentValidations
 } from '../middlewares';
 
 const router = express.Router();
 
 const { createArticle } = Article;
+const { addComment } = Comment;
 const { interestsValidator } = profileValidations;
 const {
   expectedParamsValidator,
   nonEmptyParamsValidator,
   existingTitleValidator
 } = articleValidation;
+const {
+  ensureCommentInput,
+  ensureValidComment,
+  ensureArticleExists
+} = commentValidations;
 
 router.route('/articles')
   .all(verifyToken)
@@ -24,6 +31,15 @@ router.route('/articles')
     existingTitleValidator,
     interestsValidator,
     createArticle
+  );
+
+router.route('/articles/:slug/comments')
+  .post(
+    verifyToken,
+    ensureCommentInput,
+    ensureValidComment,
+    ensureArticleExists,
+    addComment
   );
 
 export default router;
