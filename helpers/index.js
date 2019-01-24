563541f65ad85f54d4d3044200d05d0f9f2322f7
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-24 12:36:38 +0100

    [#162727567]: update exports

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,4 @@
 export { default as generateSlug } from './generateSlug';
 export { default as categories } from './categories';
 export { default as convertToArray } from './convertToArray';
+export { default as getReadTime } from './getReadTime';
