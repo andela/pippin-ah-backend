7506a033b34f8de27762120f18aaddc714cebd0a
Ebenezer <ebenezerdonu@gmail.com>
2019-01-24 16:53:11 +0100

    [#162727557] add average rating column to model and migration files

--- migrations/011-update-article-rating-column.js
+++ migrations/011-update-article-rating-column.js
@@ -7,11 +7,16 @@ module.exports = {
     queryInterface.addColumn(tableName, 'rating', {
       type: Sequelize.JSON,
       allowNull: true
+    }),
+    queryInterface.addColumn(tableName, 'aveRating', {
+      type: Sequelize.INTEGER,
+      allowNull: true
     })
   ]),
 
   down: (queryInterface, Sequelize) => Promise.all([
     queryInterface.removeColumn(tableName, 'rating'),
+    queryInterface.removeColumn(tableName, 'aveRating'),
 
     queryInterface.addColumn(tableName, 'rating', {
       type: Sequelize.INTEGER,
