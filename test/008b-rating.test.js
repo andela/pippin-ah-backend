dc8213afc3cbeff5ad3a466ac0e8bc58772f91d6
Ebenezer <ebenezerdonu@gmail.com>
2019-01-25 10:31:20 +0100

    [#162727557] add test for article rating

--- test/008b-rating.test.js
+++ test/008b-rating.test.js
@@ -8,48 +8,89 @@ const baseUrl = '/api/v1/articles';
 
 chai.use(chaiHttp);
 
-describe.only('Test Suite for Rating', () => {
+describe('Test Suite for Rating', () => {
   let nonMentorToken;
   let isMentorToken;
+  let secondUserToken;
   before(async () => {
     await models.sequelize.sync({ force: true });
 
-    const userRequestObject = {
+    const firstUserRequestObject = {
       username: 'bugsburney',
       email: 'bugsburney@email.com',
       password: 'bugsbugs'
     };
-    const articleRequestObject = {
+    const secondUserRequestObject = {
+      username: 'daffyduck',
+      email: 'daffyduck@email.com',
+      password: 'daffydaffy'
+    };
+    const firstArticleRequestObject = {
       title: 'Silicon Valley',
       body: 'Article Body',
       description: 'Article Description',
       category: 'Science'
     };
-    const loginRequestObject = {
+    const secondArticleRequestObject = {
+      title: 'Tales of Two Strangers',
+      body: 'Article Body',
+      description: 'Article Description',
+      category: 'Science'
+    };
+    const firstLoginRequestObject = {
       usernameOrEmail: 'bugsburney',
       password: 'bugsbugs'
+    };
+    const secondLoginRequestObject = {
+      usernameOrEmail: 'daffyduck',
+      password: 'daffydaffy'
     };
 
     const nonMentorResponseObject = await chai.request(server)
       .post('/api/v1/users')
-      .send(userRequestObject);
+      .send(firstUserRequestObject);
     nonMentorToken = nonMentorResponseObject.body.token;
 
     await chai.request(server)
+      .post('/api/v1/users')
+      .send(secondUserRequestObject);
+
+    await chai.request(server)
       .post(baseUrl)
-      .send(articleRequestObject)
+      .send(firstArticleRequestObject)
       .set('Authorization', nonMentorToken);
 
-    const user = await User.findOne({ where: { username: 'bugsburney' } });
-    await user.update({
+    const firstUser = await User.findOne({ where: { username: 'bugsburney' } });
+    await firstUser.update({
       isMentor: true,
       password: 'bugsbugs'
     });
 
+    const secondUser = await User.findOne({ where: { username: 'daffyduck' } });
+    await secondUser.update({
+      isMentor: true,
+      password: 'daffydaffy'
+    });
+
     const isMentorResponseObject = await chai.request(server)
       .post('/api/v1/users/login')
-      .send(loginRequestObject);
+      .send(firstLoginRequestObject);
     isMentorToken = isMentorResponseObject.body.token;
+
+    const secondUserResponseObject = await chai.request(server)
+      .post('/api/v1/users/login')
+      .send(secondLoginRequestObject);
+    secondUserToken = secondUserResponseObject.body.token;
+
+    await chai.request(server)
+      .post(baseUrl)
+      .send(secondArticleRequestObject)
+      .set('Authorization', secondUserToken);
+
+    await chai.request(server)
+      .patch(`${baseUrl}/rating/tales-of-two-strangers-daffyduck`)
+      .set('Authorization', secondUserToken)
+      .send({ rateValue: '2' });
   });
 
   describe('Rate Article', () => {
@@ -91,6 +132,37 @@ describe.only('Test Suite for Rating', () => {
         const errorMessage = 'Value must not be less than 1 or greater than 5';
         expect(response.status).to.equal(400);
         expect(response.body.error).to.equal(errorMessage);
+      });
+
+    it('should not rate an article if no value is provided',
+      async () => {
+        const response = await chai.request(server)
+          .patch(`${baseUrl}/rating/silicon-valley-bugsburney`)
+          .set('Authorization', isMentorToken)
+          .send({ rateValue: '' });
+        const errorMessage = 'Rate value must be provided';
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.equal(errorMessage);
+      });
+
+    it('should rate an article if value is valid and user is a mentor',
+      async () => {
+        const response = await chai.request(server)
+          .patch(`${baseUrl}/rating/silicon-valley-bugsburney`)
+          .set('Authorization', isMentorToken)
+          .send({ rateValue: '4' });
+        expect(response.status).to.equal(200);
+        expect(response.body.yourRating).to.equal('4');
+      });
+
+    it('should rate an article even if it already has a rating',
+      async () => {
+        const response = await chai.request(server)
+          .patch(`${baseUrl}/rating/tales-of-two-strangers-daffyduck`)
+          .set('Authorization', isMentorToken)
+          .send({ rateValue: '4' });
+        expect(response.status).to.equal(200);
+        expect(response.body.yourRating).to.equal('4');
       });
   });
 });
