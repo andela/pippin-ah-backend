240a0e6a01d8b9210239b1dc340aa4f7f2bb0150
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-26 13:08:53 +0100

    include username in author search query

--- controllers/article.js
+++ controllers/article.js
@@ -127,6 +127,9 @@ export default {
           '$User.Profile.firstName$': {
             [iLike]: `%${author}%`
           },
+          '$User.username$': {
+            [iLike]: `%${author}%`
+          },
         }
 
       });
