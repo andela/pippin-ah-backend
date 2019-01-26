df2e395f38280bfce5d89d11ff8bb731c3ca8030
Ebenezer <ebenezerdonu@gmail.com>
2019-01-26 19:01:26 +0100

    [#162727575] add date to comment object

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import moment from 'moment';
 import models from '../models';
 
 const { iLike } = Sequelize.Op;
@@ -6,12 +7,14 @@ const { Article, Comment } = models;
 
 export default {
   async addComment(req, res) {
+    const { params: { slug }, decoded: { id } } = req;
     const article = await Article.findOne({
-      where: { slug: { [iLike]: req.params.slug } }
+      where: { slug: { [iLike]: slug } }
     });
+    const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     const newComment = await Comment.create({
-      userId: req.decoded.id,
-      comment: req.body.comment,
+      userId: id,
+      comment: { [currentTime]: req.body.comment },
       articleId: article.id
     });
     return res.json({
