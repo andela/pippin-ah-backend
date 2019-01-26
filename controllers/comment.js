b46d849385d0341254076e091037cc8835e9d023
Ebenezer <ebenezerdonu@gmail.com>
2019-01-26 23:21:55 +0100

    [#162727575] add controller for editing comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -4,13 +4,13 @@ import models from '../models';
 
 const { iLike } = Sequelize.Op;
 const { Article, Comment } = models;
+const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
 export default {
   async addComment(req, res) {
     const { params: { slug }, decoded } = req;
     const article = await Article
       .findOne({ where: { slug: { [iLike]: slug } } });
-    const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     const newComment = await Comment.create({
       userId: decoded.id,
       comment: { [currentTime]: req.body.comment },
@@ -19,5 +19,16 @@ export default {
     const comment = Object.values(newComment.comment)[0];
     const { id, updatedAt } = newComment;
     return res.json({ comment, id, updatedAt });
+  },
+
+  async editComment(req, res) {
+    const { params: { id }, decoded, body: { newComment } } = req;
+    const commentRow = await Comment
+      .findOne({ where: { id, userId: decoded.id } });
+    const { comment } = commentRow;
+    comment[currentTime] = newComment;
+    await commentRow.update({ comment });
+    const updatedComment = Object.values(comment).slice(-1)[0];
+    return res.json({ updatedComment });
   }
 };
