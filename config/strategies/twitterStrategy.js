ca4d4a68808f7fc16bd8d719462138140fc2ddcd
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-27 21:24:28 +0100

    [#163498970] : refactor for mock test

--- config/strategies/twitterStrategy.js
+++ config/strategies/twitterStrategy.js
@@ -1,24 +1,35 @@
 import passport from 'passport';
 import Strategy from 'passport-twitter';
 import { Users } from '../../controllers';
+import MockStrategy from './mockStrategy';
 
 const { processSocialUser } = Users;
 
-export default {
+let strategy = new Strategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: '/api/v1/users/twitter/redirect',
+  includeEmail: true
+},
+(accessToken, refreshToken, profile, done) => {
+  done(null, {
+    email: profile.emails[0].value,
+    imageUrl: profile.photos[0].value
+  });
+});
 
+if (process.env.NODE_ENV === 'test') {
+  strategy = new MockStrategy({
+    name: 'twitter',
+    redirectURL: '/api/v1/users/twitter/redirect'
+  }, (accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+  });
+}
+
+export default {
   init() {
-    passport.use(new Strategy({
-      consumerKey: process.env.TWITTER_CONSUMER_KEY,
-      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      callbackURL: '/api/v1/users/twitter/redirect',
-      includeEmail: true
-    },
-    (accessToken, refreshToken, profile, done) => {
-      done(null, {
-        email: profile.emails[0].value,
-        imageUrl: profile.photos[0].value
-      });
-    }));
+    passport.use(strategy);
   },
 
   twitterAuthenticate: passport.authenticate(
@@ -32,5 +43,4 @@ export default {
     }),
 
   twitterOnAuthSuccess: processSocialUser
-
 };
