65475380019f6ce711b9c84031c281249bd1dc6c
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-27 21:24:20 +0100

    [#163498970] : refactor for mock test

--- config/strategies/googleStrategy.js
+++ config/strategies/googleStrategy.js
@@ -1,30 +1,39 @@
 import passport from 'passport';
 import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
 import { Users } from '../../controllers';
+import MockStrategy from './mockStrategy';
 
 const { processSocialUser } = Users;
 
-export default {
+let strategy = new GoogleStrategy({
+  clientID: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: '/api/v1/users/google/redirect'
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, {
+    email: profile.emails[0].value,
+    firstName: profile.name.givenName,
+    lastName: profile.name.familyName,
+    imageUrl: profile.photos[0].value
+  });
+});
 
+if (process.env.NODE_ENV === 'test') {
+  strategy = new MockStrategy({
+    name: 'google',
+    redirectURL: '/api/v1/users/google/redirect'
+  }, (accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+  });
+}
+
+export default {
   init() {
-    passport.use(new GoogleStrategy({
-      clientID: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: '/api/v1/users/google/redirect'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      done(null, {
-        email: profile.emails[0].value,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        imageUrl: profile.photos[0].value
-      });
-    }));
+    passport.use(strategy);
   },
 
   googleAuthenticate: passport.authenticate(
-    'google', { scope: ['profile', 'email'] }
-  ),
+    'google', { scope: ['profile', 'email'] }),
 
   googleRedirect: passport.authenticate('google',
     {
