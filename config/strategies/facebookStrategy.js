0ea8017e98bcd6e26412ba6818fdb63e0695e64d
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-27 21:24:15 +0100

    [#163498970] : refactor for mock test

--- config/strategies/facebookStrategy.js
+++ config/strategies/facebookStrategy.js
@@ -1,23 +1,33 @@
 import passport from 'passport';
 import FacebookStrategy from 'passport-facebook';
 import { Users } from '../../controllers';
-
+import MockStrategy from './mockStrategy';
 
 const { processSocialUser } = Users;
 
-// Passport methods
+let strategy = new FacebookStrategy({
+  clientID: process.env.FACEBOOK_ID,
+  clientSecret: process.env.FACEBOOK_SECRET,
+  callbackURL:
+  `${process.env.FACEBOOK_CALLBACK}/api/v1/users/facebook/redirect`,
+  profileFields: ['id', 'emails', 'name']
+},
+(accessToken, refreshToken, profile, done) => {
+  done(null, { email: profile.emails[0].value });
+});
+
+if (process.env.NODE_ENV === 'test') {
+  strategy = new MockStrategy({
+    name: 'facebook',
+    redirectURL: '/api/v1/users/facebook/redirect'
+  }, (accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+  });
+}
+
 export default {
   init() {
-    passport.use(new FacebookStrategy({
-      clientID: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-      callbackURL:
-      `${process.env.FACEBOOK_CALLBACK}/api/v1/users/facebook/redirect`,
-      profileFields: ['id', 'emails', 'name']
-    },
-    (accessToken, refreshToken, profile, done) => {
-      done(null, { email: profile.emails[0].value });
-    }));
+    passport.use(strategy);
   },
 
   fbAuthenticate: passport.authenticate(
