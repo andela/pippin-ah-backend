d7c47f5f897f164d6419ee3d8df14d909a9eab0c
Emmanuel Raji <emmanuel.raji@andela.com>
2019-01-29 17:48:18 +0100

    [#163554724]: add request controller

---
+++ controllers/request.js
@@ -0,0 +1,15 @@
+import models from '../models';
+
+const { Request } = models;
+
+export default {
+  async requestMentorship(req, res) {
+    const request = 'Request to be a mentor';
+    await Request.create({
+      userId: req.decoded.id,
+      request,
+      status: 'pending'
+    });
+    return res.send({ message: 'Your mentorship request has been sent' });
+  }
+};
