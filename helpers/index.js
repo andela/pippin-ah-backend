00424fb3833c2816ca9e836d688417779b4cd9ed
Ebenezer <ebenezerdonu@gmail.com>
2019-01-31 14:04:10 +0100

    [#163615731] add input type validator to index.js

--- helpers/index.js
+++ helpers/index.js
@@ -3,3 +3,4 @@ export { default as categories } from './categories';
 export { default as convertToArray } from './convertToArray';
 export { default as getReadTime } from './getReadTime';
 export { default as getResetMail } from './getResetMail';
+export { default as inputTypeValidator } from './inputTypeValidator';
