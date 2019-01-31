1b118fe6423df121f152e808a44290f0a6ae95ac
Ebenezer <ebenezerdonu@gmail.com>
2019-01-31 13:58:11 +0100

    [#163615731] add helper for validating input type

---
+++ helpers/inputTypeValidator.js
@@ -0,0 +1,10 @@
+export default (typeChecker = '', inputArray = '', next) => {
+  inputArray.forEach((input) => {
+    if (!typeChecker(input)) {
+      const error = new Error('Invalid input type for this param!');
+      error.status = 400;
+      return next(error);
+    }
+  });
+  return next();
+};
