78a68b648a80320d4f021d8d5cde779ba8f6778f
Ebenezer <ebenezerdonu@gmail.com>
2019-01-31 19:22:35 +0100

    [#163615731] remove inputTypeValidator import

--- helpers/index.js
+++ helpers/index.js
@@ -3,4 +3,3 @@ export { default as categories } from './categories';
 export { default as convertToArray } from './convertToArray';
 export { default as getReadTime } from './getReadTime';
 export { default as getResetMail } from './getResetMail';
-export { default as inputTypeValidator } from './inputTypeValidator';
