bab6b670cfbf0cb5b8cecf0e8409fc9e0c479b4c
Ebenezer <ebenezerdonu@gmail.com>
2019-02-01 19:48:43 +0100

    [#163660116] check if user exists in db

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,12 +1,16 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { decode } from 'punycode';
+import models from '../models';
+
+const { User } = models;
 
 dotenv.config();
 const secret = process.env.SECRET_KEY;
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
 
-  jwt.verify(token, secret, (err, decoded) => {
+  jwt.verify(token, secret, async (err, decoded) => {
     let error;
     if (!token) {
       error = new Error('No token provided');
@@ -14,6 +18,13 @@ const verifyToken = (req, res, next) => {
       return next(error);
     }
     if (err) {
+      error = new Error('Invalid token');
+      error.status = 401;
+      return next(error);
+    }
+
+    const user = await User.findOne({ where: { id: decoded.id } });
+    if (!user) {
       error = new Error('Invalid token');
       error.status = 401;
       return next(error);
