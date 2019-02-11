e874528733c64c5e9414e3d6da9875af98d5ffbe
Ebenezer <ebenezerdonu@gmail.com>
2019-02-11 13:17:29 +0100

    [feature: #162727574] fix typeError in cover image migration file

--- migrations/020-update-article-add-cover-img.js
+++ migrations/020-update-article-add-cover-img.js
@@ -1,10 +1,10 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface
-    .addColumn('Article', 'coverImageUrl', {
+    .addColumn('Articles', 'coverImageUrl', {
       type: Sequelize.BOOLEAN,
       defaultValue: false
     }),
   // eslint-disable-next-line no-unused-vars
   down: (queryInterface, Sequelize) => queryInterface
-    .removeColumn('Article', 'coverImageUrl')
+    .removeColumn('Articles', 'coverImageUrl')
 };
