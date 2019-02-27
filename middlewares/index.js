9e33a9c1842247772cc32f313c42d2ba970f5daa
Ebenezer <ebenezerdonu@gmail.com>
2019-02-27 17:04:05 +0100

    [chore: #164276773] add middleware for getting user id on get article

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,7 +2,7 @@ export { default as errorHandler } from './errorHandler';
 export { default as userValidations } from './userValidations';
 export { default as profileValidations } from './profileValidations';
 export { default as notFoundRoute } from './notFoundRoute';
-export { default as verifyToken } from './authentication';
+export { verifyToken, getUserId } from './authentication';
 export { default as articleValidation } from './articleValidation';
 export { default as followValidations } from './followValidations';
 export { default as commentValidations } from './commentValidations';
